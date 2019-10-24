455682191b6afdb6a867bf252dc82d4efbde59ff
Mark SIMONI <mark.simoni@ngu.no>
2019-10-24 14:09:19 +0200

    update example

--- example.py
+++ example.py
@@ -3,8 +3,8 @@ def add(a, b):
 
 
 def test_add():
-    assert add(2, 3) == 5
-    assert add('space', 'ship') == 'spaceship'
+    assert add(2, 3) == 5 # call the add function with these parameters
+    assert add('space', 'ship') == 'spaceship' # call the add function with these parameters
 
 
 def subtract(a, b):
